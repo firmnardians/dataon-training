f50d261dcbc4acd063d89fa9eb9eacadc1fe1b3c
firmnardians <4647804-firmnardians@users.noreply.gitlab.com>
2023-11-23 01:15:49 +0700

    feat: add about and home pages

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from '../components/Container';
+
+function AboutPage() {
+	return (
+		<Container>
+			<div>
+				<h1>New Employee Training - React.js & GIT.</h1>
+				<p>Trainer: Ade Firman</p>
+				<p>Location: Dataon Office</p>
+
+				<smal>23 November 2023</smal>
+			</div>
+		</Container>
+	);
+}
+
+export default AboutPage;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../components/Container';
+
+function HomePage() {
+	return (
+		<Container>
+			<div>
+				<p>Daftar kehadiran:</p>
+			</div>
+		</Container>
+	);
+}
+
+export default HomePage;
