92feab90a9badd5c79a8b1b053af6942fed40da3
abid313 <abidfajar50@gmail.com>
2025-05-08 15:29:23 +0700

    add API test

--- src/pages/TestAPI.js
+++ src/pages/TestAPI.js
@@ -3,33 +3,32 @@ import Container from '../components/Container';
 
 function TestAPI() {
 	const [users, setUsers] = useState([]);
-	const [isLoading, setIsLoading] = useState(true); // State untuk loading
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		// Fetch data dari API
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then((response) => response.json())
 			.then((data) => {
 				setUsers(data);
-				setIsLoading(false); // Set loading ke false setelah data diterima
+				setIsLoading(false);
 			})
 			.catch((error) => {
 				console.error('Error fetching data:', error);
-				setIsLoading(false); // Set loading ke false meskipun ada error
+				setIsLoading(false);
 			});
 	}, []);
 
 	return (
 		<Container>
 			<div>
-				{isLoading ? ( // Tampilkan loading jika isLoading true
+				{isLoading ? (
 					<p>Loading...</p>
 				) : (
 					<>
 						<p>Data Users:</p>
 						<ol>
 							{users
-								.filter((user) => user.website.includes('.org')) // Filter email yang mengandung ".org"
+								.filter((user) => user.website.includes('.org'))
 								.map((user) => (
 									<li key={user.id}>
 										<p>Name: {user.username}</p>
