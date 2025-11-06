41179b0c1a11cada4cca129a9bd5a4af06e32e6b
alvira09 <alvirdewi.10@gmail.com>
2025-11-06 14:48:42 +0700

    tambah nama

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import Container from '../components/Container';
+import React from "react";
+import Container from "../components/Container";
 
 function HomePage() {
-	return (
-		<Container>
-			<div>
-				<p>Daftar kehadiran:</p>
-				<ol>
-					<li>Ade Firman</li>
-					<li>Nur Ahmad Khatim</li>
-					<li>Rahman Dhika</li>
-					<li>Aris Aryanto</li>
-					<li>Zakiy Almaulidani Ruwenda</li>
-					<li>Arya Febian</li>
-					<li>Toni</li>
-					<li>Aldien</li>
-					<li>Abimanyu Luthfi</li>
-					<li>Christian Winata</li>
-					<li>Ghifari</li>
-					<li>Salman</li>
-					<li>Abid</li>
-				</ol>
-			</div>
-		</Container>
-	);
+  return (
+    <Container>
+      <div>
+        <p>Daftar kehadiran:</p>
+        <ol>
+          <li>Ade Firman</li>
+          <li>Nur Ahmad Khatim</li>
+          <li>Rahman Dhika</li>
+          <li>Aris Aryanto</li>
+          <li>Zakiy Almaulidani Ruwenda</li>
+          <li>Arya Febian</li>
+          <li>Toni</li>
+          <li>Aldien</li>
+          <li>Abimanyu Luthfi</li>
+          <li>Christian Winata</li>
+          <li>Ghifari</li>
+          <li>Salman</li>
+          <li>Abid</li>
+          <li>Alvira</li>
+        </ol>
+      </div>
+    </Container>
+  );
 }
 
 export default HomePage;
