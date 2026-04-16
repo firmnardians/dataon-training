c37ce4f2d4acfbc3d3b56cb5daf84a5ab181f28a
renoalane <renopradino@gmail.com>
2026-04-16 11:10:46 +0700

    add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,41 +2,33 @@ import React from "react";
 import Container from "../components/Container";
 
 function HomePage() {
-return (
-		<Container>
-			<div>
-				<p>Daftar kehadiran:</p>
-				<ol>
-					<li>Ade Firman</li>
-					<li>Nur Ahmad Khatim</li>
-					<li>Rahman Dhika</li>
-					<li>Aris Aryanto</li>
-					<li>Zakiy Almaulidani Ruwenda</li>
-					<li>Arya Febian</li>
-					<li>Toni</li>
-					<li>Aldien</li>
-					<li>Abimanyu Luthfi</li>
-					<li>Christian Winata</li>
-					<li>Ghifari</li>
-					<li>Salman</li>
-					<li>Abid</li>
-					<li>Fachrul Ramdaniar H</li>
-<<<<<<< HEAD
+  return (
+    <Container>
+      <div>
+        <p>Daftar kehadiran:</p>
+        <ol>
+          <li>Ade Firman</li>
+          <li>Nur Ahmad Khatim</li>
+          <li>Rahman Dhika</li>
+          <li>Aris Aryanto</li>
+          <li>Zakiy Almaulidani Ruwenda</li>
+          <li>Arya Febian</li>
+          <li>Toni</li>
+          <li>Aldien</li>
+          <li>Abimanyu Luthfi</li>
+          <li>Christian Winata</li>
+          <li>Ghifari</li>
+          <li>Salman</li>
+          <li>Abid</li>
+          <li>Fachrul Ramdaniar H</li>
           <li>Malinda</li>
           <li>Alvira</li>
-          <li>Reno</li>
-||||||| 6a2c7ea
-          <li>Malinda</li>
-          <li>Alvira</li>
-=======
-					<li>Malinda</li>
-					<li>Alvira</li>
-					<li>Charisty</li>
->>>>>>> 51a856e8fa679b4fcff18c1ffd84ab62da376009
-				</ol>
-			</div>
-		</Container>
-	);
+          <li>Charisty</li>
+          <li>Irfan Saprudin</li>
+        </ol>
+      </div>
+    </Container>
+  );
 }
 
 export default HomePage;
